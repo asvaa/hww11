755bb2ef3c3b3acb53f79d3388bcc85f88d57f1b
Adam Mysaev <adammysaev@MacBook-Pro-Adam.local>
2025-05-01 20:32:14 +0300

    first commit

--- src/s2-homeworks/hw10/bll/store.ts
+++ src/s2-homeworks/hw10/bll/store.ts
@@ -4,14 +4,16 @@ import { themeReducer } from '../../hw12/bll/themeReducer'
 
 const reducers = combineReducers({
     loading: loadingReducer, // hw10
-    theme: themeReducer, // hw12
+    theme: themeReducer,     // hw12
 })
 
 const store = legacy_createStore(reducers)
 
 export default store
 
+// Типы
 export type AppStoreType = ReturnType<typeof reducers>
+export type AppDispatch = typeof store.dispatch
 
 // @ts-ignore
-window.store = store // for dev // для того чтобы автотесты видели состояние данных
+window.store = store // for dev
