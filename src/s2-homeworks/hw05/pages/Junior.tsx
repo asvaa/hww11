f56dbe8303989820760fd48cec503ca7125b801f
Adam Mysaev <adammysaev@MacBook-Pro-Adam.local>
2025-05-02 16:55:51 +0300

    HW

--- src/s2-homeworks/hw05/pages/Junior.tsx
+++ src/s2-homeworks/hw05/pages/Junior.tsx
@@ -1,22 +1,22 @@
-import React from 'react'
- import HW6 from '../../hw06/HW6'
- import HW7 from '../../hw07/HW7'
- import HW8 from '../../hw08/HW8'
- import HW9 from '../../hw09/HW9'
-import HW10 from '../../hw10/HW10'
-// import HW5 from '../HW5'
+import React from "react";
+import HW6 from "../../hw06/HW6";
+import HW7 from "../../hw07/HW7";
+import HW8 from "../../hw08/HW8";
+import HW9 from "../../hw09/HW9";
+import HW10 from "../../hw10/HW10";
+import HW5 from "../HW5";
 
 function Junior() {
-    return (
-        <div id={'hw5-page-junior'}>
-            {/* <HW5 /> */}
-            <HW6 />
-            <HW7 />
-            <HW8 />
-            <HW9 />
-            <HW10/>
-        </div>
-    )
+  return (
+    <div id={"hw5-page-junior"}>
+      <HW5 />
+      <HW6 />
+      <HW7 />
+      <HW8 />
+      <HW9 />
+      <HW10 />
+    </div>
+  );
 }
 
-export default Junior
+export default Junior;
