37c3ede4cb1f35d8741ba732388d343147edcecc
Adam Mysaev <adammysaev@MacBook-Pro-Adam.local>
2025-05-15 21:07:42 +0300

    GG

--- src/s2-homeworks/hw05/layout/Layout.tsx
+++ src/s2-homeworks/hw05/layout/Layout.tsx
@@ -1,30 +1,31 @@
-import React, { FC, ReactNode, useEffect, useState } from "react";
-import { Header } from "../header/Header";
-import { Sidebar } from "../sidebar/Sidebar";
-import { useLocation } from "react-router-dom";
-import { PATH } from "../Pages";
+import React, { FC, ReactNode, useEffect, useState } from 'react'
+import { Header } from '../header/Header'
+import { Sidebar } from '../sidebar/Sidebar'
 
 type PropsType = {
-  children: ReactNode;
-};
+    children: ReactNode
+}
 
 export const Layout: FC<PropsType> = ({ children }) => {
-  const location = useLocation();
-  const isHW10 = location.pathname === PATH.HW10;
+    const [open, setOpen] = useState(false)
+    const handleClose = () => setOpen(false)
+    const handleOpen = () => setOpen(true)
 
-  const [open, setOpen] = useState(false);
-  const handleClose = () => setOpen(false);
-  const handleOpen = () => setOpen(true);
+    console.log(open);
+    
+    useEffect(() => {
+        open && (document.body.style.overflow = 'hidden')
+        !open && (document.body.style.overflow = 'unset')
+    }, [open]) // отключает прокрутку при открытом меню
 
-  useEffect(() => {
-    document.body.style.overflow = open ? "hidden" : "unset";
-  }, [open]);
-
-  return (
-    <>
-      {!isHW10 && <Sidebar open={open} handleClose={handleClose} />}
-      <Header open={open} handleOpen={handleOpen} handleClose={handleClose} />
-      <div>{children}</div>
-    </>
-  );
-};
+    return (
+        <>
+            <Sidebar open={open} handleClose={handleClose} />
+            <Header open={open} handleOpen={handleOpen} handleClose={handleClose} />
+            <div>
+                {/*страницы*/}
+                {children}
+            </div>
+        </>
+    )
+}
