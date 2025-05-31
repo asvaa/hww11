340361097ca531713d57bfe5380881e1fbd8d8dd
Adam Mysaev <adammysaev@MacBook-Pro-Adam.local>
2025-05-31 13:27:22 +0300

    JJ

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -4,19 +4,12 @@ import s2 from "../../s1-main/App.module.css";
 import { restoreState } from "../hw06/localStorage/localStorage";
 import SuperRange from "./common/c7-SuperRange/SuperRange";
 
-/*
- * 1 - передать значения в оба слайдера
- * 2 - дописать типы и логику функции change
- * 3 - сделать стили в соответствии с дизайном
- * */
-
 function HW11() {
-  const [value1, setValue1] = useState(restoreState<number>("hw11-value1", 0));
-  const [value2, setValue2] = useState(
-    restoreState<number>("hw11-value2", 100)
-  );
+  const [value1, setValue1] = useState(restoreState<number>("hw11-value1", 25));
+  const [value2, setValue2] = useState(restoreState<number>("hw11-value2", 75));
 
   const change = (_event: Event, value: number | number[]) => {
+    console.log("Slider changed:", value);
     if (Array.isArray(value)) {
       setValue1(value[0]);
       setValue2(value[1]);
@@ -39,6 +32,9 @@ function HW11() {
               id={"hw11-single-slider"}
               value={value1}
               onChange={change}
+              min={0}
+              max={100}
+              step={1}
             />
           </div>
           <div className={s.wrapper}>
@@ -49,6 +45,9 @@ function HW11() {
               id={"hw11-double-slider"}
               value={[value1, value2]}
               onChange={change}
+              min={0}
+              max={100}
+              step={1}
             />
             <span id={"hw11-value-2"} className={s.number}>
               {value2}
