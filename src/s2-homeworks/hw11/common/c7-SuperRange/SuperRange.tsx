a731409c4e4a877b8e7b965b50091ecc47cff891
Adam Mysaev <adammysaev@MacBook-Pro-Adam.local>
2025-06-02 11:18:15 +0300

    Hello

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -12,45 +12,49 @@ const SuperRange: React.FC<SliderProps> = ({
   const isSingle = id === "hw11-single-slider";
 
   return (
-    <Slider
-      {...rest}
-      id={id}
-      aria-label={ariaLabel}
-      value={rest.value}
-      onChange={onChange}
-      onChangeCommitted={onChangeCommitted}
-      componentsProps={{
-        thumb: {
-          className: isSingle ? "thumb-single" : "thumb-double",
-        },
-      }}
-      sx={{
-        width: "100%",
-        maxWidth: 375,
-        height: 4,
-        padding: 0,
-        margin: 0,
-        boxSizing: "border-box",
-        color: "#52af77",
-        "& .MuiSlider-thumb": {
-          height: 20,
-          width: 20,
-          backgroundColor: "#fff",
-          border: "2px solid #52af77",
-          boxShadow: "none",
-          "&:hover, &.Mui-focusVisible, &.Mui-active": {
-            boxShadow: "none",
+    <div
+      data-testid={isSingle ? "slider-single-wrapper" : "slider-double-wrapper"}
+    >
+      <Slider
+        {...rest}
+        id={id}
+        aria-label={ariaLabel}
+        value={rest.value}
+        onChange={onChange}
+        onChangeCommitted={onChangeCommitted}
+        componentsProps={{
+          thumb: {
+            className: isSingle ? "thumb-single" : "thumb-double",
           },
-        },
-        "& .MuiSlider-track": {
-          border: "none",
-        },
-        "& .MuiSlider-rail": {
-          opacity: 1,
-          backgroundColor: "#bfbfbf",
-        },
-      }}
-    />
+        }}
+        sx={{
+          width: "100%",
+          maxWidth: 375,
+          height: 4,
+          padding: 0,
+          margin: 0,
+          boxSizing: "border-box",
+          color: "#52af77",
+          "& .MuiSlider-thumb": {
+            height: 20,
+            width: 20,
+            backgroundColor: "#fff",
+            border: "2px solid #52af77",
+            boxShadow: "none",
+            "&:hover, &.Mui-focusVisible, &.Mui-active": {
+              boxShadow: "none",
+            },
+          },
+          "& .MuiSlider-track": {
+            border: "none",
+          },
+          "& .MuiSlider-rail": {
+            opacity: 1,
+            backgroundColor: "#bfbfbf",
+          },
+        }}
+      />
+    </div>
   );
 };
 
