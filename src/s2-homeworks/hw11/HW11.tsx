eb016f59bf37a27b5fa445c2eab60f04441820c9
Adam Mysaev <adammysaev@MacBook-Pro-Adam.local>
2025-06-17 22:10:04 +0300

    MM

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -1,123 +1,63 @@
-import React, { useEffect, useState } from "react";
-import s from "./HW11.module.css";
-import s2 from "../../s1-main/App.module.css";
-import { restoreState, saveState } from "../hw06/localStorage/localStorage";
-import SuperRange from "./common/c7-SuperRange/SuperRange";
+import React, { useState } from 'react'
+import s from './HW11.module.css'
+import s2 from '../../s1-main/App.module.css'
+import { restoreState } from '../hw06/localStorage/localStorage'
+import SuperRange from './common/c7-SuperRange/SuperRange'
+
+/*
+* 1 - передать значения в оба слайдера
+* 2 - дописать типы и логику функции change
+* 3 - сделать стили в соответствии с дизайном
+* */
 
 function HW11() {
-  const initialSingleValue =
-    process.env.NODE_ENV === "test" ? 25 : restoreState("hw11-value1", 0);
+    // for autotests // не менять // можно подсунуть в локалСторэдж нужные числа, чтоб увидеть как они отображаются
+    const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
+    const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
 
-  const initialDoubleValue =
-    process.env.NODE_ENV === "test"
-      ? [25, 75]
-      : restoreState("hw11-value2", [0, 100]);
-
-  const [valueSingle, setValueSingle] = useState<number>(initialSingleValue);
-  const [valueDouble, setValueDouble] = useState<[number, number]>(
-    initialDoubleValue as [number, number]
-  );
-
-  useEffect(() => {
-    if (process.env.NODE_ENV === "test") {
-      localStorage.removeItem("hw11-value1");
-      localStorage.removeItem("hw11-value2");
-      saveState("hw11-value1", 25);
-      saveState("hw11-value2", [25, 75]);
+    const change = (event: any, value: any) => {
+        // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
+        console.log(value);
+        
+        if(Array.isArray(value)) {
+          setValue1(value[0])
+          setValue2(value[1])
+        } else {
+          setValue1(value)
+        }
     }
-  }, []);
 
-  const handleSingleChange = (
-    _e: Event | React.SyntheticEvent,
-    val: number | number[]
-  ) => {
-    if (typeof val === "number") {
-      setValueSingle(val);
-      saveState("hw11-value1", val);
+    return (
+        <div id={'hw11'}>
+            <div className={s2.hwTitle}>Homework #11</div>
 
-      setValueDouble(([_, right]) => {
-        const newDouble: [number, number] = [val, right];
-        saveState("hw11-value2", newDouble);
-        return newDouble;
-      });
-    }
-  };
+            <div className={s2.hw}>
+                <div className={s.container}>
+                    <div className={s.wrapper}>
+                        <span id={'hw11-value'} className={s.number}>{value1}</span>
+                        <SuperRange
+                            id={'hw11-single-slider'}
+                            onChange={change}
+                            value={value1}
+                            // сделать так чтоб value1 изменялось // пишет студент
 
-  const handleDoubleChange = (
-    _e: Event | React.SyntheticEvent,
-    val: number | number[]
-  ) => {
-    if (Array.isArray(val) && val.length === 2) {
-      const newDouble: [number, number] = [val[0], val[1]];
-      setValueDouble(newDouble);
-      saveState("hw11-value2", newDouble);
+                        />
+                    </div>
+                    <div className={s.wrapper}>
+                        <span id={'hw11-value-1'} className={s.number}>{value1}</span>
+                        <SuperRange
+                            id={'hw11-double-slider'}
+                            onChange={change}
+                            value={[value1, value2]}
+                            // сделать так чтоб value1/2 изменялось // пишет студент
 
-      setValueSingle(newDouble[0]);
-      saveState("hw11-value1", newDouble[0]);
-    }
-  };
-
-  return (
-    <div data-testid="hw11">
-      <div className={s2.hwTitle}>Hometask №11</div>
-      <div className={s2.hw}>
-        <div className={s.container}>
-          {/* ---------- ОДИНОЧНЫЙ ---------- */}
-          <div className={s.wrapper}>
-            <div data-testid="hw11-single-slider" className={s.sliderWrapper}>
-              <span
-                id="hw11-value"
-                data-testid="hw11-value-single"
-                className={s.number}
-              >
-                {valueSingle}
-              </span>
-              <SuperRange
-                id="hw11-single-slider"
-                value={valueSingle}
-                onChange={handleSingleChange}
-                onChangeCommitted={handleSingleChange}
-                min={0}
-                max={100}
-                step={1}
-                data-testid="hw11-single-slider-input"
-              />
+                        />
+                        <span id={'hw11-value-2'} className={s.number}>{value2}</span>
+                    </div>
+                </div>
             </div>
-          </div>
-
-          {/* ---------- ДВОЙНОЙ ---------- */}
-          <div className={s.wrapper}>
-            <div data-testid="hw11-double-slider" className={s.sliderWrapper}>
-              <span
-                id="hw11-value-1"
-                data-testid="hw11-value-double-1"
-                className={s.number}
-              >
-                {valueDouble[0]}
-              </span>
-              <SuperRange
-                id="hw11-double-slider"
-                value={valueDouble}
-                onChange={handleDoubleChange}
-                onChangeCommitted={handleDoubleChange}
-                min={0}
-                max={100}
-                step={1}
-                data-testid="hw11-double-slider-input"
-              />
-              <span
-                id="hw11-value-2"
-                data-testid="hw11-value-double-2"
-                className={s.number}
-              >
-                {valueDouble[1]}
-              </span>
-            </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    )
 }
 
-export default HW11;
+export default HW11
