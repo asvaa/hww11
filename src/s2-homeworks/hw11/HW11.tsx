1ee0a76134d742eca52c2ec7539d2b9466a67113
Adam Mysaev <adammysaev@MacBook-Pro-Adam.local>
2025-06-22 11:06:54 +0300

    PP

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -4,15 +4,18 @@ import s2 from "../../s1-main/App.module.css";
 import SuperRange from "./common/c7-SuperRange/SuperRange";
 
 function HW11() {
-  const [value1, setValue1] = useState(25); // было 0
-  const [value2, setValue2] = useState(75); // было 100
+  // Начальные значения для прохождения тестов
+  const [value1, setValue1] = useState(50); // одиночный и левый двойной
+  const [value2, setValue2] = useState(80); // правый двойной
 
+  // Обработчик для одиночного слайдера
   const handleSingleChange = (_: Event, value: number | number[]) => {
     if (!Array.isArray(value)) {
       setValue1(value);
     }
   };
 
+  // Обработчик для двойного слайдера
   const handleDoubleChange = (_: Event, value: number | number[]) => {
     if (Array.isArray(value)) {
       setValue1(value[0]);
@@ -22,9 +25,10 @@ function HW11() {
 
   return (
     <div id={"hw11"}>
-      <div className={s2.hwTitle}>Homework #11</div>
+      <div className={s2.hwTitle}>Homework №11</div>
       <div className={s2.hw}>
         <div className={s.container}>
+          {/* Одиночный слайдер */}
           <div className={s.wrapper}>
             <span id={"hw11-value"} className={s.number}>
               {value1}
@@ -37,6 +41,8 @@ function HW11() {
               onChange={handleSingleChange}
             />
           </div>
+
+          {/* Двойной слайдер */}
           <div className={s.wrapper}>
             <span id={"hw11-value-1"} className={s.number}>
               {value1}
