58a24c8ffef7823a66efe78d3675cc0504852d52
Adam Mysaev <adammysaev@MacBook-Pro-Adam.local>
2025-06-23 19:02:58 +0300

    YY

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -7,12 +7,6 @@ const SuperRange: React.FC<SliderProps> = ({ sx, ...props }) => {
       sx={{
         width: 300,
         color: "#00CC22",
-        "& .MuiSlider-thumb": {
-          height: 24,
-          width: 24,
-          backgroundColor: "#fff",
-          border: "2px solid currentColor",
-        },
         ...sx,
       }}
       {...props}
