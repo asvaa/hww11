402ac4410a49cac3514de8bdfae7501445d39c6e
Adam Mysaev <adammysaev@MacBook-Pro-Adam.local>
2025-06-23 17:57:55 +0300

    TT

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -1,45 +1,60 @@
 import React, { useState } from "react";
 import s from "./HW11.module.css";
 import s2 from "../../s1-main/App.module.css";
+import { restoreState } from "../hw06/localStorage/localStorage";
 import SuperRange from "./common/c7-SuperRange/SuperRange";
 
+/*
+ * 1 - передать значения в оба слайдера
+ * 2 - дописать типы и логику функции change
+ * 3 - сделать стили в соответствии с дизайном
+ * */
+
 function HW11() {
-  const [value1, setValue1] = useState(0);
-  const [value2, setValue2] = useState(100);
+  // for autotests // не менять // можно подсунуть в локалСторэдж нужные числа, чтоб увидеть как они отображаются
+  const [value1, setValue1] = useState(restoreState<number>("hw11-value1", 0));
+  const [value2, setValue2] = useState(
+    restoreState<number>("hw11-value2", 100)
+  );
 
-  const handleSingleChange = (event: any, value: number | number[]) => {
-    setValue1(value as number);
-    setValue2(prev => (value as number) > prev ? (value as number) : prev);
-  };
-
-  const handleDoubleChange = (event: any, value: number | number[]) => {
-    if (Array.isArray(value)) {
+  const change = (event: any, value: any) => {
+    if (!Array.isArray(value)) {
+      setValue1(value);
+      setValue2((prev) => (value > prev ? value : prev));
+    } else {
       setValue1(value[0]);
       setValue2(value[1]);
     }
   };
 
   return (
-    <div id="hw11">
+    <div id={"hw11"}>
       <div className={s2.hwTitle}>Homework #11</div>
+
       <div className={s2.hw}>
         <div className={s.container}>
           <div className={s.wrapper}>
-            <span id="hw11-value" className={s.number}>{value1}</span>
+            <span id={"hw11-value"} className={s.number}>
+              {value1}
+            </span>
             <SuperRange
-              id="hw11-single-slider"
+              id={"hw11-single-slider"}
               value={value1}
-              onChange={handleSingleChange}
+              onChange={change}
             />
           </div>
           <div className={s.wrapper}>
-            <span id="hw11-value-1" className={s.number}>{value1}</span>
+            <span id={"hw11-value-1"} className={s.number}>
+              {value1}
+            </span>
             <SuperRange
-              id="hw11-double-slider"
+              id={"hw11-double-slider"}
               value={[value1, value2]}
-              onChange={handleDoubleChange}
+              onChange={change}
             />
-            <span id="hw11-value-2" className={s.number}>{value2}</span>
+            <span id={"hw11-value-2"} className={s.number}>
+              {value2}
+            </span>
           </div>
         </div>
       </div>
--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -1,28 +1,33 @@
-import Slider from "@mui/material/Slider";
-import { SliderProps } from "@mui/material/Slider";
+import React from "react";
+import { Slider, SliderProps } from "@mui/material";
 
 const SuperRange: React.FC<SliderProps> = (props) => {
   return (
-    <Slider
-      sx={{
-        color: "#00FF00",
-        height: 4,
-        "& .MuiSlider-thumb": {
-          height: 18,
-          width: 18,
-          backgroundColor: "#fff",
-          border: "2px solid #00FF00",
-        },
-        "& .MuiSlider-rail": {
-          color: "#C4C4C4",
-          opacity: 1,
-        },
-        "& .MuiSlider-track": {
-          color: "#00FF00",
-        },
-      }}
-      {...props}
-    />
+  <Slider
+  sx={{
+    color: '#00FF00', // зелёный цвет активной части (можно использовать твой цвет)
+    height: 4,
+    '& .MuiSlider-thumb': {
+      height: 18,
+      width: 18,
+      backgroundColor: '#fff',
+      border: '2px solid #00FF00', // зелёная обводка ползунка
+      boxShadow: 'none',
+      '&:hover, &.Mui-focusVisible, &.Mui-active': {
+        boxShadow: '0 0 0 6px rgba(0,255,0,0.16)',
+      },
+    },
+    '& .MuiSlider-rail': {
+      color: '#C4C4C4', // неактивная часть (серенькая)
+      opacity: 1,
+    },
+    '& .MuiSlider-track': {
+      color: '#00FF00', // активная часть (зелёная)
+    },
+  }}
+  {...props}
+/>
+
   );
 };
 
