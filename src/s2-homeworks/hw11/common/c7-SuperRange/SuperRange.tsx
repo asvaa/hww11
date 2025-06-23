be7d111a41360055222eb1a96d2030944d9a1f61
Adam Mysaev <adammysaev@MacBook-Pro-Adam.local>
2025-06-23 15:59:18 +0300

    EE

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -1,4 +1,4 @@
-import { Slider, SliderProps } from "@mui/material";
+import Slider, {  SliderProps } from "@mui/material/Slider";
 
 const SuperRange: React.FC<SliderProps> = (props) => {
   return (
