ae736dbb88493ca9135f4afdfcf1caab7b1980a5
Adam Mysaev <adammysaev@MacBook-Pro-Adam.local>
2025-06-23 18:24:36 +0300

    OO

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -4,58 +4,55 @@ import s2 from "../../s1-main/App.module.css";
 import { restoreState } from "../hw06/localStorage/localStorage";
 import SuperRange from "./common/c7-SuperRange/SuperRange";
 
+/*
+ * 1 - передать значения в оба слайдера
+ * 2 - дописать типы и логику функции change
+ * 3 - сделать стили в соответствии с дизайном
+ * */
+
 function HW11() {
+  // for autotests // не менять // можно подсунуть в локалСторэдж нужные числа, чтоб увидеть как они отображаются
   const [value1, setValue1] = useState(restoreState<number>("hw11-value1", 0));
-  const [value2, setValue2] = useState(restoreState<number>("hw11-value2", 100));
+  const [value2, setValue2] = useState(
+    restoreState<number>("hw11-value2", 100)
+  );
 
-  const handleSingleChange = (event: unknown, newValue: number | number[]) => {
-    if (typeof newValue === "number") {
-      setValue1(newValue);
-    }
-  };
-
-  const handleDoubleChange = (event: unknown, newValue: number | number[]) => {
-    if (Array.isArray(newValue)) {
-      setValue1(newValue[0]);
-      setValue2(newValue[1]);
+  const change = (_event: unknown, value: number | number[]) => {
+    if (!Array.isArray(value)) {
+      setValue1(value);
+      setValue2((prev) => (value > prev ? value : prev));
+    } else {
+      setValue1(value[0]);
+      setValue2(value[1]);
     }
   };
 
   return (
-    <div id={"hw11"} data-testid="hw11">
+    <div id={"hw11"}>
       <div className={s2.hwTitle}>Homework #11</div>
 
       <div className={s2.hw}>
         <div className={s.container}>
-          <div className={s.wrapper} data-testid="single-slider-wrapper">
-            <span id={"hw11-value"} className={s.number} data-testid="hw11-value">
+          <div className={s.wrapper}>
+            <span id={"hw11-value"} className={s.number}>
               {value1}
             </span>
-            <div id="hw11-single-slider" data-testid="single-slider">
-              <SuperRange
-                value={value1}
-                onChange={handleSingleChange}
-                min={0}
-                max={100}
-                step={1}
-              />
-            </div>
+            <SuperRange
+              id={"hw11-single-slider"}
+              value={value1}
+              onChange={change}
+            />
           </div>
-
-          <div className={s.wrapper} data-testid="double-slider-wrapper">
-            <span id={"hw11-value-1"} className={s.number} data-testid="hw11-value-1">
+          <div className={s.wrapper}>
+            <span id={"hw11-value-1"} className={s.number}>
               {value1}
             </span>
-            <div id="hw11-double-slider" data-testid="double-slider">
-              <SuperRange
-                value={[value1, value2]}
-                onChange={handleDoubleChange}
-                min={0}
-                max={100}
-                step={1}
-              />
-            </div>
-            <span id={"hw11-value-2"} className={s.number} data-testid="hw11-value-2">
+            <SuperRange
+              id={"hw11-double-slider"}
+              value={[value1, value2]}
+              onChange={change}
+            />
+            <span id={"hw11-value-2"} className={s.number}>
               {value2}
             </span>
           </div>
