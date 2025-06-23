36a0a77d37f22f4e2edcb114dd65867af58c417e
Adam Mysaev <adammysaev@MacBook-Pro-Adam.local>
2025-06-23 18:49:28 +0300

    NN

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -1,70 +1,64 @@
-import React, { useState } from "react";
-import s from "./HW11.module.css";
-import s2 from "../../s1-main/App.module.css";
-import { restoreState } from "../hw06/localStorage/localStorage";
-import SuperRange from "./common/c7-SuperRange/SuperRange";
+import React, { useState } from 'react';
+import s from './HW11.module.css';
+import s2 from '../../s1-main/App.module.css';
+import { restoreState } from '../hw06/localStorage/localStorage';
+import SuperRange from './common/c7-SuperRange/SuperRange';
 
 function HW11() {
-  const [value1, setValue1] = useState(restoreState<number>("hw11-value1", 0));
-  const [value2, setValue2] = useState(
-    restoreState<number>("hw11-value2", 100)
-  );
+    const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0));
+    const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100));
 
-  const handleSingleSliderChange = (
-    event: Event,
-    newValue: number | number[]
-  ) => {
-    if (typeof newValue === "number") {
-      setValue1(newValue);
-    }
-  };
+    const handleSingleChange = (event: Event, newValue: number | number[]) => {
+        if (typeof newValue === 'number') {
+            setValue1(newValue);
+        }
+    };
 
-  const handleDoubleSliderChange = (
-    event: Event,
-    newValue: number | number[]
-  ) => {
-    if (Array.isArray(newValue)) {
-      // Добавляем проверку минимального расстояния
-      if (newValue[1] - newValue[0] >= 5) {
-        setValue1(newValue[0]);
-        setValue2(newValue[1]);
-      }
-    }
-  };
+    const handleDoubleChange = (event: Event, newValue: number | number[]) => {
+        if (Array.isArray(newValue)) {
+            // Гарантируем, что первый ползунок не превышает второй
+            const [newVal1, newVal2] = newValue;
+            if (newVal1 < newVal2) {
+                setValue1(newVal1);
+                setValue2(newVal2);
+            } else if (newValue[0] !== value1) {
+                // Если двигаем первый ползунок
+                setValue1(Math.min(newVal1, value2 - 1));
+            } else if (newValue[1] !== value2) {
+                // Если двигаем второй ползунок
+                setValue2(Math.max(newVal2, value1 + 1));
+            }
+        }
+    };
 
-  return (
-    <div id={"hw11"}>
-      <div className={s2.hwTitle}>Homework #11</div>
+    return (
+        <div id={'hw11'}>
+            <div className={s2.hwTitle}>Homework #11</div>
 
-      <div className={s2.hw}>
-        <div className={s.container}>
-          <div className={s.wrapper}>
-            <span id={"hw11-value"} className={s.number}>
-              {value1}
-            </span>
-            <SuperRange
-              id={"hw11-single-slider"}
-              value={value1}
-              onChange={handleSingleSliderChange}
-            />
-          </div>
-          <div className={s.wrapper}>
-            <span id={"hw11-value-1"} className={s.number}>
-              {value1}
-            </span>
-            <SuperRange
-              id={"hw11-double-slider"}
-              value={[value1, value2]}
-              onChange={handleDoubleSliderChange}
-            />
-            <span id={"hw11-value-2"} className={s.number}>
-              {value2}
-            </span>
-          </div>
+            <div className={s2.hw}>
+                <div className={s.container}>
+                    <div className={s.wrapper}>
+                        <span id={'hw11-value'} className={s.number}>{value1}</span>
+                        <SuperRange
+                            id={'hw11-single-slider'}
+                            value={value1}
+                            onChange={handleSingleChange}
+                        />
+                    </div>
+                    <div className={s.wrapper}>
+                        <span id={'hw11-value-1'} className={s.number}>{value1}</span>
+                        <SuperRange
+                            id={'hw11-double-slider'}
+                            value={[value1, value2]}
+                            onChange={handleDoubleChange}
+                            disableSwap
+                        />
+                        <span id={'hw11-value-2'} className={s.number}>{value2}</span>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default HW11;
--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -7,6 +7,12 @@ const SuperRange: React.FC<SliderProps> = ({ sx, ...props }) => {
       sx={{
         width: 300,
         color: "#00CC22",
+        "& .MuiSlider-thumb": {
+          height: 24,
+          width: 24,
+          backgroundColor: "#fff",
+          border: "2px solid currentColor",
+        },
         ...sx,
       }}
       {...props}
