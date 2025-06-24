7d389af901f79e1cab3a111e2d8122f49ff259da
Adam Mysaev <adammysaev@MacBook-Pro-Adam.local>
2025-06-24 11:33:29 +0300

    NN

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -4,15 +4,7 @@ import s2 from "../../s1-main/App.module.css";
 import { restoreState } from "../hw06/localStorage/localStorage";
 import SuperRange from "./common/c7-SuperRange/SuperRange";
 
-/*
- * 1 - передать значения в оба слайдера
- * 2 - дописать типы и логику функции change
- * 3 - сделать стили в соответствии с дизайном
- * */
-
 function HW11() {
-  // for autotests // не менять // можно подсунуть в локалСторэдж нужные числа, чтоб увидеть как они отображаются
-
   const [value1, setValue1] = useState(restoreState<number>("hw11-value1", 0));
   const [value2, setValue2] = useState(
     restoreState<number>("hw11-value2", 100)
@@ -20,19 +12,17 @@ function HW11() {
 
   const change = (event: Event, newValue: number | number[]) => {
     if (Array.isArray(newValue)) {
-      // Для диапазонного слайдера
       setValue1(newValue[0]);
       setValue2(newValue[1]);
     } else {
-      // Для обычного слайдера
       setValue1(newValue);
+      setValue2((prev) => (newValue > prev ? newValue : prev));
     }
   };
 
   return (
     <div id={"hw11"}>
       <div className={s2.hwTitle}>Homework #11</div>
-
       <div className={s2.hw}>
         <div className={s.container}>
           <div className={s.wrapper}>
--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -1,17 +1,30 @@
-import React from "react";
-import Slider, { SliderProps } from "@mui/material/Slider";
+import React from 'react'
+import Slider, { SliderProps } from '@mui/material/Slider'
 
 const SuperRange: React.FC<SliderProps> = ({ sx, ...props }) => {
-  return (
-    <Slider
-      sx={{
-        width: 300,
-        color: "#00CC22",
-        ...sx,
-      }}
-      {...props}
-    />
-  );
-};
+    return (
+        <Slider
+            sx={{
+                width: 300,
+                color: '#00CC22',
+                '& .MuiSlider-thumb': {
+                    height: 24,
+                    width: 24,
+                    backgroundColor: '#fff',
+                    border: '2px solid currentColor',
+                    '&:focus, &:hover, &.Mui-active': {
+                        boxShadow: '0 0 0 8px rgba(0, 204, 34, 0.16)',
+                    },
+                },
+                '& .MuiSlider-rail': {
+                    opacity: 0.5,
+                    backgroundColor: '#bfbfbf',
+                },
+                ...sx,
+            }}
+            {...props}
+        />
+    )
+}
 
-export default SuperRange;
+export default SuperRange
