54c363e95f135643d481f04416fbdd69fecd8d8a
Adam Mysaev <adammysaev@MacBook-Pro-Adam.local>
2025-06-28 13:07:36 +0300

    YY

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -18,10 +18,10 @@ function HW11() {
   //   restoreState<number>("hw11-value2", 100)
   // );
   const [value1, setValue1] = useState(
-    Number(restoreState<number>("hw11-value1",0))
+    restoreState<number>("hw11-value1",0)
   );
   const [value2, setValue2] = useState(
-    Number(restoreState<number>("hw11-value2", 100)) 
+    restoreState<number>("hw11-value2", 100)
   );
 
   const change = (event: Event, newValue: number | number[]) => {
